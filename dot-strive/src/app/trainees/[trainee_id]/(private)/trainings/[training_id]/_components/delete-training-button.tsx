f94e23985a05fc032e67f9aede695c1c8164a3a7
gn-t-k <gntk1995422@gmail.com>
2023-07-09 12:18:28 +0900

    トレーニングを削除する

---
+++ dot-strive/src/app/trainees/[trainee_id]/(private)/trainings/[training_id]/_components/delete-training-button.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/app/_components/button";
+import { stack } from "styled-system/patterns";
+
+import { deleteTraining } from "../_repositories/delete-training";
+
+import type { Training } from "@/app/_schemas/training";
+import type { Result } from "neverthrow";
+import type { FC, MouseEventHandler } from "react";
+
+type Props = {
+  traineeId: string;
+  training: Training;
+  afterDelete: AfterDelete;
+};
+type AfterDelete = (result: Result<Training, Error>) => void;
+export const DeleteTrainingButton: FC<Props> = (props) => {
+  const [isConfirming, setIsConfirming] = useState(false);
+  const onClickDelete: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+
+    setIsConfirming(true);
+  };
+  const onClickExecute: MouseEventHandler<HTMLButtonElement> = async (
+    event
+  ) => {
+    event.preventDefault();
+
+    const result = await deleteTraining({
+      traineeId: props.traineeId,
+      trainingId: props.training.id,
+    });
+    setIsConfirming(false);
+    props.afterDelete(result);
+  };
+  const onClickCancel: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+
+    setIsConfirming(false);
+  };
+
+  return isConfirming ? (
+    <div className={stack({ direction: "row" })}>
+      <Button onClick={onClickExecute} visual="negative">
+        {new Date(props.training.date).toLocaleString("ja", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+        })}
+        のトレーニングを削除する
+      </Button>
+      <Button onClick={onClickCancel}>キャンセル</Button>
+    </div>
+  ) : (
+    <Button onClick={onClickDelete}>削除する</Button>
+  );
+};
