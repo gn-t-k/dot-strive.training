6a45cbd99735819d3f85be9813a6aedfe513a409
gn-t-k <gntk1995422@gmail.com>
2023-07-23 11:59:22 +0900

    wip

--- dot-strive/src/app/trainees/[trainee_id]/(private)/muscles/[muscle_id]/page.tsx
+++ dot-strive/src/app/trainees/[trainee_id]/(private)/muscles/[muscle_id]/page.tsx
@@ -1,8 +1,10 @@
+import { endOfWeek, format, startOfWeek } from "date-fns";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 import { Loading } from "@/app/_components/loading";
+import { getFetcher } from "@/app/_utils/get-fetcher";
 import { stack } from "styled-system/patterns";
 
 import { DeleteMuscle } from "./_components/delete-muscle";
@@ -11,7 +13,7 @@ import { MuscleDetail } from "./_components/muscle-detail";
 import type { NextPage } from "@/app/_types/page";
 import type { Route } from "next";
 
-const Page: NextPage = (props) => {
+const Page: NextPage = async (props) => {
   const traineeId = props.params?.["trainee_id"];
   if (!traineeId) {
     redirect("/" satisfies Route);
@@ -22,9 +24,21 @@ const Page: NextPage = (props) => {
     redirect(to satisfies Route<typeof to>);
   }
 
+  const today = new Date();
+  const from = startOfWeek(today);
+  const to = endOfWeek(today);
+  const data = await getFetcher()(
+    `/api/trainees/${traineeId}/trainings/muscles/${muscleId}/dates/${format(
+      from,
+      "yyyy-MM-dd"
+    )}/${format(to, "yyyy-MM-dd")}`
+  );
+  const trainings = await data.json();
+
   return (
     <section className={stack({ direction: "column" })}>
       <h1>部位詳細</h1>
+      <p>{JSON.stringify(trainings)}</p>
       <Suspense fallback={<Loading description="部位データを取得しています" />}>
         <MuscleDetail {...{ traineeId, muscleId }} />
         <Link href={`/trainees/${traineeId}/muscles/${muscleId}/edit`}>
