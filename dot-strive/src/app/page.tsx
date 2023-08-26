61048a546cb5a024f63bff6563c754d9be7c9676
gn-t-k <gntk1995422@gmail.com>
2023-08-27 07:24:18 +0900

    loading調整

--- dot-strive/src/app/page.tsx
+++ dot-strive/src/app/page.tsx
@@ -1,27 +1,12 @@
 import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth";
 
-import { nextAuthOptions } from "@/app/_libs/next-auth/options";
-import { prisma } from "@/app/_libs/prisma/client";
+import { getTraineeOrRedirect } from "./_actions/get-trainee-or-redirect";
 
 import type { NextPage } from "@/app/_types/page";
 import type { Route } from "next";
 
 const Page: NextPage = async () => {
-  const session = await getServerSession(nextAuthOptions);
-  const authUserId = session?.user.id;
-  if (!authUserId) {
-    redirect("/login" satisfies Route);
-  }
-
-  const trainee = await prisma.trainee.findUnique({
-    where: {
-      authUserId,
-    },
-  });
-  if (!trainee) {
-    redirect("/trainees/onboarding" satisfies Route);
-  }
+  const trainee = await getTraineeOrRedirect();
 
   const to = `/trainees/${trainee.id}/trainings` as const;
   redirect(to satisfies Route<typeof to>);
--- dot-strive/src/app/trainees/[trainee_id]/(private)/trainings/[training_id]/edit/loading.tsx
+++
@@ -1,8 +0,0 @@
-import { Loading } from "@/app/_components/loading";
-
-import type { FC } from "react";
-
-const PageLoading: FC = () => {
-  return <Loading description="データを取得しています" />;
-};
-export default PageLoading;
