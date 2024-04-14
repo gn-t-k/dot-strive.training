a9a3f12169befe5969bcde43fac9a2f7d0c8ac26
Gentoku Murakami <54714779+gn-t-k@users.noreply.github.com>
2024-04-14 17:24:28 +0900

    ヘッダーにナビゲーションを移した (#50)

--- app/routes/trainees.$traineeId/header-navigation.tsx
+++ app/routes/trainees.$traineeId/header-navigation.tsx
@@ -1,22 +1,57 @@
-import { CircleUserRound } from "lucide-react";
+import { CircleUserRound, Dumbbell, History, Tag } from "lucide-react";
 
+import { Link } from "@remix-run/react";
 import { Logotype } from "app/ui/logotype";
 import type { FC } from "react";
 
 type Props = {
   traineeId: string;
+  location: string;
 };
-export const HeaderNavigation: FC<Props> = ({ traineeId }) => {
+export const HeaderNavigation: FC<Props> = ({ traineeId, location }) => {
   return (
-    <nav className="inline-flex w-full items-center justify-between bg-white py-2 pl-4 pr-1">
-      <a href="/">
+    <nav className="flex w-full items-center justify-between bg-white py-2 px-4">
+      <div>
         <Logotype />
-      </a>
-      <a href={`/trainees/${traineeId}`}>
-        <div className="p-2">
-          <CircleUserRound size="20px" />
-        </div>
-      </a>
+      </div>
+      <div className="flex w-full items-center justify-end">
+        <Link to={`/trainees/${traineeId}/trainings`} className="p-2">
+          <History
+            size="20px"
+            className={
+              location === "trainings"
+                ? "text-primary"
+                : "text-muted-foreground"
+            }
+          />
+        </Link>
+        <Link to={`/trainees/${traineeId}/exercises`} className="p-2">
+          <Dumbbell
+            size="20px"
+            className={
+              location === "exercises"
+                ? "text-primary"
+                : "text-muted-foreground"
+            }
+          />
+        </Link>
+        <Link to={`/trainees/${traineeId}/tags`} className="p-2">
+          <Tag
+            size="20px"
+            className={
+              location === "tags" ? "text-primary" : "text-muted-foreground"
+            }
+          />
+        </Link>
+        <Link to={`/trainees/${traineeId}`} className="p-2">
+          <CircleUserRound
+            size="20px"
+            className={
+              location === "" ? "text-primary" : "text-muted-foreground"
+            }
+          />
+        </Link>
+      </div>
     </nav>
   );
 };
