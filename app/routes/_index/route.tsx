ad67d86c012a68ad559e97e4f4ade6094435140e
Gentoku Murakami <54714779+gn-t-k@users.noreply.github.com>
2024-10-19 18:45:01 +0900

    【wip】loaderまわりとか、skeltonの置き場所とか修正 (#495)

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,6 +1,13 @@
 import { type LoaderFunctionArgs, redirect } from "@remix-run/cloudflare";
 import { getAuthenticator } from "app/features/auth/get-authenticator.server";
 
+/**
+ * dot-strive.training/にアクセスした場合
+ * - ログインしている場合はトレーニング一覧ページにリダイレクト
+ * - ログインしていない場合はログインページにリダイレクト
+ *
+ * TODO: ここにLT置きたい
+ */
 export const loader = async ({ context, request }: LoaderFunctionArgs) => {
   const authenticator = getAuthenticator(context, request);
   const trainee = await authenticator.isAuthenticated(request);
